ea6606334036c39ea74e6495a2f2a14c87acc84c
ananliangliang <ananliangliang@vip.qq.com>
2023-08-24 23:22:25 +0800

    feat: 增加根据路由激活菜单

--- cool-frontend/src/app/app.component.ts
+++ cool-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+    private router: Router,
+  ) {
+  }
+
+
   title = 'cool';
+
+
+
+
+  getRouteUrl() { return this.router.url }
+
+  protected readonly console = console;
+  protected readonly Router = Router;
 }
